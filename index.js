764b0ecdf3c6e1c748e39dffc5a2f9c4fe744035
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-05-08 21:38:18 +0300

    test extract media

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ async function trySendMessage(){
         mainEmbed.addFields( { name: key, value: value } );
     }
 
+    extractMedia(text);
+
     let embeds = new Array();
     let images = extractImageURLs(text);
     if (images.length > 0){
@@ -263,4 +265,26 @@ function deleteGitComments(text){
     const commentRegex = /<!--[\s\S]*?-->|<!--[\s\S]*/gm;
 
     return text.replaceAll(commentRegex, '').trim();
+}
+
+/**
+ * @param {string} text 
+ * @returns {string[]}
+ */
+async function extractMedia(text){
+    const urlRegex = /(http|https):\/\/[^)\]\s]+/gm;
+
+    let mediaArray = new Array();
+    let i = 0;
+    let result;
+    while((result = urlRegex.exec(text)) != null){
+        let url = result[0];
+        let responce = await fetch(url, {
+            method: 'HEAD'
+        });
+        var type = responce.headers.get('Content-Type');
+        console.log(type);
+    }
+
+    return mediaArray;
 }
