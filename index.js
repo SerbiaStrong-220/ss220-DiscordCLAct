d4e28f04827f84ab4a2095e32c3ea1306f71d611
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-05-09 10:48:55 +0300

    add dirname

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { error, getInput, setFailed, warning } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 import { AttachmentBuilder, Embed, EmbedBuilder, WebhookClient } from 'discord.js';
+import { fileURLToPath } from 'url';
 import fs from 'fs';
-import { url } from 'inspector';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const webhook_id = getInput('webhook_id');
 const webhook_token = getInput('webhook_token');
